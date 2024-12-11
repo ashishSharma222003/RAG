1bdb6ecbd57ce591524e2e28cc09e96a038955dc
Ashish Sharma <01234aag@gmail.com>
2024-12-11 17:23:59 +0530

    added ingestion pipline

--- document_embedder.py
+++ document_embedder.py
@@ -4,6 +4,10 @@ from llama_index.core import VectorStoreIndex,SimpleDirectoryReader
 from llama_index.core.ingestion import IngestionPipeline
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.extractors import TitleExtractor
+from llama_index.core import SimpleKeywordTableIndex, VectorStoreIndex
+from llama_index.core.storage.docstore import SimpleDocumentStore
+
+
 num_workers=1
 
 # loads BAAI/bge-small-en-v1.5
@@ -12,14 +16,21 @@ embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 Settings.embed_model=embed_model
 
 documents = SimpleDirectoryReader(input_dir="./data").load_data(show_progress=True,num_workers=num_workers)
-transformations = [
-    SentenceSplitter(chunk_size=512, chunk_overlap=128),
-    TitleExtractor(nodes=5),
-]
+pipeline = IngestionPipeline(
+    transformations=[
+        SentenceSplitter(chunk_size=512,chunk_overlap=256),
+        TitleExtractor(nodes=3),
+        HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"),
+    ],
+)
+nodes=pipeline.run(documents=documents,num_workers=num_workers)
 
 
-index=VectorStoreIndex.from_documents(documents=documents,show_progress=True,transformations=transformations)
-index.storage_context.persist()
+vector_index = VectorStoreIndex(nodes)
+keyword_index = SimpleKeywordTableIndex(nodes)
+# index=VectorStoreIndex.from_documents(documents=documents,show_progress=True,transformations=transformations)
+vector_index.storage_context.persist(persist_dir="./storage")
+keyword_index.storage_context.persist(persist_dir="./strage_key")
 
 
 
