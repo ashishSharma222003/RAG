9cab4be3e1e4b48f1874ac6f5b0745d69b5ab620
Ashish Sharma <01234aag@gmail.com>
2024-12-11 21:18:23 +0530

    changed the llm to llama3.2

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 from llama_index.core import StorageContext,load_index_from_storage,Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.llms.huggingface import HuggingFaceLLM
+# from llama_index.llms.huggingface import HuggingFaceLLM
 from llama_index.core import SimpleKeywordTableIndex, VectorStoreIndex
 from CustomRetriever import CustomRetriever
 # Retrievers
@@ -16,14 +16,22 @@ from llama_index.core.indices.query.query_transform.base import (
 )
 from llama_index.core.postprocessor import LLMRerank
 from llama_index.core.query_engine import MultiStepQueryEngine
-from llama_index.core.query_engine import SubQuestionQueryEngine
- 
+# from llama_index.core.query_engine import SubQuestionQueryEngine
+from llama_index.core import set_global_tokenizer
+from transformers import AutoTokenizer
+from llama_index.llms.ollama import Ollama
+
+
 #jina-embeddings-v3: Multilingual Embeddings | dimension=1048
-embed_model = HuggingFaceEmbedding(model_name="jinaai/jina-embeddings-v3")
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5",trust_remote_code=True)
 Settings.embed_model=embed_model
 #setting up Hugging face LLM Zephyr-7b-alpha
-llm = HuggingFaceLLM(model_name="HuggingFaceH4/zephyr-7b-alpha")
+# llm = HuggingFaceLLM(model_name="HuggingFaceH4/zephyr-7b-alpha",tokenizer_name="HuggingFaceH4/zephyr-7b-beta")
+llm = Ollama(model="llama3.2:3b", request_timeout=120.0)
 Settings.llm=llm
+# set_global_tokenizer(
+#     AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-alpha").encode
+# )
 
 
 storage_context=StorageContext.from_defaults(persist_dir="./storage")
@@ -32,15 +40,15 @@ storage_context_key=StorageContext.from_defaults(persist_dir="./storage_key")
 #loading index from storage for vectorindex and for keywordindex
 vector_index = load_index_from_storage( storage_context=storage_context)
 keyword_index = load_index_from_storage( storage_context=storage_context_key)
+print("load index")
 
-
-vector_retriever = VectorIndexRetriever(index=vector_index, similarity_top_k=5)
+vector_retriever = VectorIndexRetriever(index=vector_index, similarity_top_k=10)
 keyword_retriever = KeywordTableSimpleRetriever(index=keyword_index)
 
-
+print("***retriever***")
 #Hybrid search combining keyword and semantic search techniques
 custom_retriever = CustomRetriever(vector_retriever, keyword_retriever)
-
+print("***custom retriever***")
 # define response synthesizer
 response_synthesizer = get_response_synthesizer(response_mode="tree_summarize")
 
@@ -55,9 +63,14 @@ custom_query_engine = RetrieverQueryEngine(
         )
     ]
 )
+print("*********"*5)
 #query decomposition convert q question into 3 sub query
 query_engine = MultiStepQueryEngine(
-    custom_query_engine, query_transform=step_decompose_transform
+    custom_query_engine, query_transform=step_decompose_transform,index_summary="you are the best giver"
 )
-
+print('query_engine')
+print("**********"*5)
+if __name__=="__main__":
+    response=query_engine.query("Why Is Blue Ocean Strategy of Rising Importance?")
+    print(response)
 
--- document_embedder.py
+++ document_embedder.py
@@ -2,24 +2,35 @@ from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import Settings
 from llama_index.core import VectorStoreIndex,SimpleDirectoryReader
 from llama_index.core.ingestion import IngestionPipeline
-from llama_index.core.node_parser import SentenceSplitter
-from llama_index.core.extractors import TitleExtractor
+from llama_index.core.node_parser import SentenceSplitter,TokenTextSplitter
+from llama_index.core.extractors import TitleExtractor,SummaryExtractor 
 from llama_index.core import SimpleKeywordTableIndex, VectorStoreIndex
 from llama_index.core.storage.docstore import SimpleDocumentStore
+# from llama_index.llms.huggingface import HuggingFaceLLM
+from llama_index.llms.ollama import Ollama
+from llama_index.core import set_global_tokenizer
+from transformers import AutoTokenizer
 
 
-num_workers=1
+num_workers=1#can't increase due pc resource constraints
 
+llm = Ollama(model="llama3.2:3b", request_timeout=120.0)
+Settings.llm=llm
+set_global_tokenizer(
+    AutoTokenizer.from_pretrained("HuggingFaceH4/zephyr-7b-alpha").encode
+)
 # loads jinaai/jina-embeddings-v3
-embed_model = HuggingFaceEmbedding(model_name="jinaai/jina-embeddings-v3")
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5",trust_remote_code=True)
 
 Settings.embed_model=embed_model
-
+print("*"*80)
 documents = SimpleDirectoryReader(input_dir="./data",recursive=True).load_data(show_progress=True,num_workers=num_workers)
 pipeline = IngestionPipeline(
     transformations=[
-        SentenceSplitter(chunk_size=512,chunk_overlap=256),
-        TitleExtractor(nodes=3),
+        TokenTextSplitter(chunk_size=256, chunk_overlap=128),
+        # SentenceSplitter(chunk_size=256,chunk_overlap=256),
+        # TitleExtractor(nodes=3),                                      #Can't use this extractor since they consume all my pc resource and starts to 
+        # SummaryExtractor(), 
         HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5"),
     ],
 )
@@ -30,7 +41,7 @@ vector_index = VectorStoreIndex(nodes)
 keyword_index = SimpleKeywordTableIndex(nodes)
 # index=VectorStoreIndex.from_documents(documents=documents,show_progress=True,transformations=transformations)
 vector_index.storage_context.persist(persist_dir="./storage")
-keyword_index.storage_context.persist(persist_dir="./strage_key")
+keyword_index.storage_context.persist(persist_dir="./storage_key")
 
 
 
